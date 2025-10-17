440451a605bcec1af9e7edf057a3336792a0dca8
Matthew Davis <real059mattd@gmail.com>
2025-10-16 22:39:15 -0500

    Kit giubg ib'

--- LouisasAstroWebsite/Program.cs
+++ LouisasAstroWebsite/Program.cs
@@ -1,10 +1,36 @@
 using LouisasAstroWebsite.Components;
+using LouisasAstroWebsite.Data;
+using MongoDB.Bson;
+using MongoDB.Driver;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
+
+//MongoDB
+var mongoConn = builder.Configuration.GetConnectionString("MongoDb");
+builder.Services.AddSingleton<IMongoClient>(new MongoClient(mongoConn));
+builder.Services.AddScoped(x =>
+{
+    var client = x.GetRequiredService<IMongoClient>();
+    var database = client.GetDatabase("GirlfriendDB");
+
+    // Check if collection exists, create if not
+    var filter = new BsonDocument();
+    var collections = database.ListCollectionsAsync(new ListCollectionsOptions { Filter = filter }).Result;
+    var collectionNames = collections.ToList().Select(c => c["name"].AsString);
+    if (!collectionNames.Contains("users"))
+    {
+        database.CreateCollection("users");
+    }
+
+    return database.GetCollection<User>("users");
+});
+
+// Add API controllers
+builder.Services.AddControllers();
 
 var app = builder.Build();
 
